2ffd9a6f3f912a1b53a4ca7138fdc3d2e547921f
Gabriel <frodollface@gmail.com>
2021-08-17 15:50:22 -0400

    Volviendo a trabajar

--- src/Componentes/BarraAbajo.js
+++ src/Componentes/BarraAbajo.js
@@ -4,23 +4,52 @@ import handjob from "./imagenes/handjob.gif";
 import Estadisticas from "./Estadisticas";
 import Makeup from "./imagenes/Makeup.png";
 import Bimbofication from "./imagenes/Bimbofication.png";
-import Selfie from "./imagenes/Selfie.png";
+import hearth from "./imagenes/hearth.png";
 import First_main from "./imagenes/First_main.png";
 import First_maindos from "./imagenes/First_maindos.png"
+import Hipnotizada from "./imagenes/Hipnotizada.png"
 
 export default class BarraAbajo extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      Imagenes: [First_main,First_maindos,Makeup,Bimbofication],
+      Imagenes: [First_main,First_maindos,Makeup,Hipnotizada,Bimbofication],
+      img: hearth,
+      value: 0
     };
+    this.Click_corazon = this.Click_corazon.bind(this);
   }
 
   Character_transformation(Transformation){
+    if(Transformation - this.state.value == 1){
+      this.setState({value: this.state.value + 1})
+    }
+    
     return(
-      <img src={this.state.Imagenes[Transformation]} className={Estilos.main_character} alt = {"a"} />
+      <img src={this.state.Imagenes[this.state.value]} className={Estilos.main_character} alt = {"a"} />
     )
   }
+  Click_corazon = (e) => {
+    var Imag = document.createElement("img");
+    Imag.src = this.state.img;
+    Imag.className = "subir_level";
+    Imag.style.top= 548 + 'px';
+    Imag.style.left= 110 + 'px';
+    document.body.appendChild(Imag);
+    Imag.addEventListener("animationend", function () {
+    Imag.parentElement.removeChild(Imag);
+    });
+    var Image = document.createElement("img");
+    Image.src = this.state.img;
+    Image.className = "subir_level_dos";
+    Image.style.top= 548 + 'px';
+    Image.style.left= 110 + 'px';
+
+    document.body.appendChild(Image);
+    Image.addEventListener("animationend", function () {
+    Image.parentElement.removeChild(Image);
+    });
+  };
 
 
 
@@ -28,23 +57,16 @@ export default class BarraAbajo extends React.Component {
     
     return (
       <div className={Estilos.contenido}>
-        <div>
-          <table>
-            <tr>
-              <td>
-                {" "}
-                <img src={handjob} className={Estilos.handjob} alt = {""}  />
-              </td>
-              <td>
-                <Estadisticas Values={this.props.Valores} />
-              </td>
-              <td className={Estilos.central_contenido}></td>
-              <td>
-                {this.Character_transformation(this.props.Valores.Bimbofication)}
-              </td>
-            </tr>
-          </table>
-        </div>
+        
+        <div className = "row">
+          <div className = "col-md-3"><button  className ={Estilos.btnn} onClick = {() =>{
+            this.props.Clicking_dick()
+            this.Click_corazon();
+
+          }}><img src = {handjob} className ={Estilos.handjob}></img></button></div>
+          <div className = "col-md-4 padiando"><Estadisticas Values={this.props.Valores}/></div>
+          <div className = "col-md-5 alin">{this.Character_transformation(this.props.Valores.Bimbofication)}</div>
+        </div> 
       </div>
     );
   }
